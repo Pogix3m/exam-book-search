a8bffbdfb4dd304589e3ec4a577301b47f01b94f
Harold Niones <harold_x3m@yahoo.com>
2024-05-15 23:34:49 +0800

    Add source

--- src/library/documenter/request.ts
+++ src/library/documenter/request.ts
@@ -3,6 +3,7 @@ import HTTP, { IHTTP } from "@pogix3m/http";
 import { TResponse } from "./types";
 import { ErrorFormatter } from "./error";
 
+// source: https://documenter.getpostman.com/view/15757300/UzXNVJCg
 const url: string = "https://ejditq67mwuzeuwrlp5fs3egwu0yhkjz.lambda-url.us-east-2.on.aws/api";
 
 export const request: IHTTP<TResponse> = new HTTP<TResponse>(
